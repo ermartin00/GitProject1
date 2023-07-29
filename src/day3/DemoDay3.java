484c94704cd9be2e4f48a491ec7d571dd463e806
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-29 11:04:04 -0400

    string uppercase S

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -3,7 +3,7 @@ package day3;
 public class DemoDay3 {
 	
 	public static void main(String[] args) {
-		string country = "USA";
+		String country = "USA";
 		String state = "VA";
 		
 		System.out.println("Ernesto");
