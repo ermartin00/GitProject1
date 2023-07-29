a912867a7062aef7ef17c5128420d3f43e0f1b2e
ermartin00 <140658480+ermartin00@users.noreply.github.com>
2023-07-29 13:35:40 -0400

    Update DemoDay5.java

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -9,5 +9,9 @@ public class DemoDay5 {
 	public static void getInfo() {
 		System.out.println("today is Java/Github day...");
 	}
+
+	public static void getInfo1() {
+		System.out.println("Java/Github day...");
+	}
 	
 }
